d06f2e1cb3a63e29d422d67340ec596b2cf5d785
CsToMy <cstompet@gmail.com>
2024-12-04 18:52:00 +0100

    Added Hello World project.

---
+++ app.go
@@ -0,0 +1,7 @@
+package main // package definition, like namespace in c++ or C#
+
+import "fmt" // using one of the standard library
+
+func main() {
+	fmt.Print("Hello World!")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module example.hu/hello
+
+go 1.23.4
